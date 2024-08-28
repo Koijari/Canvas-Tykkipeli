54d8141808c84afc8c1c00a158ffeff2aed115c7
Janne Kolari <janne.kolari@esedulainen.fi>
2024-08-28 20:40:16 +0300

    Perusasettelut

--- tykki.js
+++ tykki.js
@@ -1,1 +1,15 @@
 // tykkipeli
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    document.body.innerHTML = '';
+    var peliAlue = {
+        canvas : document.createElement("canvas"),
+        aloita : function() {
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+            this.context = this.canvas.getContext("2d");
+            document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        }
+    }
+    peliAlue.aloita();
+})
